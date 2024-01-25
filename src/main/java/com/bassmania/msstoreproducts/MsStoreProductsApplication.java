9d57e140b31cfe9a2ce3df5855681b049098a2ef
Salva Roca <salva.roca@icloud.com>
2024-01-25 11:09:25 +0100

    Configuration for deployment on Railway

--- src/main/java/com/bassmania/msstoreproducts/MsStoreProductsApplication.java
+++ src/main/java/com/bassmania/msstoreproducts/MsStoreProductsApplication.java
@@ -1,12 +1,17 @@
 package com.bassmania.msstoreproducts;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class MsStoreProductsApplication {
 
     public static void main(String[] args) {
+        // Retrieve execution profile from environment variable. If not present, default profile is selected.
+        String profile = System.getenv("PROFILE");
+        System.setProperty("spring.profiles.active", profile != null ? profile : "default");
         SpringApplication.run(MsStoreProductsApplication.class, args);
     }
 
